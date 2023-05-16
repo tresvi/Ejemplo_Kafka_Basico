45802dff43aae89021cef9f0389b308f9f4c0d97
tresvi <69699106+tresvi@users.noreply.github.com>
2023-05-16 00:16:30 -0300

    Se mejora ejemplo

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Confluent.Kafka;
 using Newtonsoft.Json;
 
+Console.WriteLine("Ejecutando Consumer simple...");
 
 var config = new ConsumerConfig
 {
@@ -20,17 +21,22 @@ try
     while (true)
     {
         var response = consumer.Consume(token.Token);
-        if (response.Message == null)
+        if (response.Message != null)
         {
+            Console.WriteLine($"Llego un msje!: {response.Message.Value}");
             var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
             Console.WriteLine($"State: {weather.State}, Temp: {weather.Temperature} ºC");
         }
     }
 }
-catch (Exception)
+catch (Exception ex)
 {
-
+    Console.Error.WriteLine($"Error: {ex.Message}");
     throw;
 }
+finally { consumer.Close(); }
+
 
 public record Weather(string State, int Temperature);
+
+
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Confluent.Kafka;
 using Newtonsoft.Json;
 
+//FUENTE: https://www.youtube.com/watch?v=A4Y7z6wFRk0
+
 var config = new ProducerConfig { 
     BootstrapServers = "localhost:9092", 
     Acks = Acks.Leader
@@ -11,14 +13,13 @@ using var producer = new ProducerBuilder<Null, string>(config).Build();
 try
 {
     string? state;
+    Console.WriteLine("Ingrese msjes para enviar a la cola:");
     while ((state = Console.ReadLine()) != null)
     {
         var response = await producer.ProduceAsync("weather-topic",
             new Message<Null, string> { Value = JsonConvert.SerializeObject(
                 new Weather(state, 70))});
     }
-
-    
 }
 catch (ProduceException<Null, string> ex)
 {
@@ -30,3 +31,34 @@ public record Weather(string State, int Temperature)
 {
 
 }
+
+
+/*Hacerlo todo en consolas separadas y dejarlas corriendo
+ * 
+ 0)Instalacion: https://www.apache.org/dyn/closer.cgi?path=/kafka/2.1.0/kafka_2.11-2.1.0.tgz   (instalar el JDK de Java 8 tambien) 
+ 
+1)Levantar coordinador de transaccion "Zookeeper" (en puerto 2181 por defecto):
+PS C:\tools\kafka_2.11-2.1.0> .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties 
+
+2) Levantar kafka (puerto 9092)
+PS C:\tools\kafka_2.11-2.1.0> .\bin\windows\kafka-server-start.bat .\config\server.properties
+
+3) Crear un topico
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+
+4)Verificar si se creó (se listan TODOS los topicos activos) 
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-topics.bat --list --zookeeper localhost:2181
+
+5)Crear un producer. Aca ya se pueden ingresar lineas de texto al topico
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-console-producer.bat --broker-list localhost:9092 --topic test
+
+6)Crear consumer (se recibiran los msjes enviados antes)
+PS C:\tools\kafka_2.11-2.1.0\bin\windows> .\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic test --from-beginning
+
+
+Desde Docker: Es recontra pesado:
+
+1) curl --silent --output docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/6.1.1-post/cp-all-in-one/docker-compose.yml
+2) docker-compose up -d
+
+*/
