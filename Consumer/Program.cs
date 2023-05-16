aeaa4e44d64e32cec4b5ebd64055cd98368af097
Raul Palavecino <raulpalav@gmail.com>
2023-05-15 22:22:37 -0300

    Initial commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message == null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, Temp: {weather.Temperature} ºC");
+        }
+    }
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+public record Weather(string State, int Temperature);
